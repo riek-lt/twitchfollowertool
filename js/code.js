a45813ea738d74e8e75ed09098af68ecac7abe27
Riekelt <riekelt@hotmail.com>
2020-05-12 22:14:51 +0200

    Convert CSV to JSON

--- js/code.js
+++ js/code.js
@@ -1,37 +1,64 @@
 var csvContents = [];
 var accountInfo = [];
-var i=0;
+var i, j = 0;
+var jsonList;
 
 document.getElementById('input-file')
   .addEventListener('change', getFile)
 
 function getFile(event) {
-	const input = event.target
+  const input = event.target
   if ('files' in input && input.files.length > 0) {
-	  placeFileContent(
+    placeFileContent(
       document.getElementById('content-target'),
       input.files[0])
   }
 }
 
 function placeFileContent(target, file) {
-	readFileContent(file).then(content => {
-  	target.value = content
+  readFileContent(file).then(content => {
+    target.value = content;
 
-csvContents = content.split('\n');
-for (i=0; i < csvContents.length; i++) {
-  accountInfo = csvContents[i].split(',');
-  console.log(accountInfo[0]);
-}
+    jsonList = csvJSON(content);
+    jsonList = JSON.parse(jsonList);
 
+    console.log(jsonList[0].userName);
+
+    csvContents = content.split('\n');
+    for (i = 0; i < csvContents.length; i++) {
+      accountInfo = csvContents[i].split(',');
+      // console.log(accountInfo[0]);
+    }
   }).catch(error => console.log(error))
 }
 
 function readFileContent(file) {
-	const reader = new FileReader()
+  const reader = new FileReader()
   return new Promise((resolve, reject) => {
     reader.onload = event => resolve(event.target.result)
     reader.onerror = error => reject(error)
     reader.readAsText(file)
   })
+}
+
+function csvJSON(csv) {
+  let lines = csv.split("\n");
+  let result = [];
+  let headers = lines[0].split(",");
+
+  for (i = 1; i < lines.length; i++) {
+
+    var obj = {};
+    var currentline = lines[i].split(",");
+
+    for (j = 0; j < headers.length; j++) {
+      obj[headers[j]] = currentline[j];
+    }
+
+    result.push(obj);
+
+  }
+
+  //return result; //JavaScript object
+  return JSON.stringify(result); //JSON
 }
